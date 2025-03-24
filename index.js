d86edf18eb494870815122c0ce689b19fd8e8e4f
expmo <53528719+EXPSTUDIOmo@users.noreply.github.com>
2025-03-24 10:24:23 +0100

    Added comments

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
-const Max = require('max-api');
-const PORT = 5005;
-
+// The Max npm package to communicate with our maxpatch
+const Max = require('max-api'); 
+const PORT = 5005; // Port that our server will run on. Default HTTP : Port 80, Default HTTPS: Port 443
 
 Max.addHandler('hello', () => {
     // this code runs, when max sends hello
     Max.outlet(5);
 });
 
-const express = require('express');
-const http = require('http');
-const path = require('path');
+const express = require('express'); // express package that handles most of serverside-routing and data handling
+const http = require('http'); // module to work with the HTTP protocol to send / receive Hyper-Text
+const path = require('path'); // to access filepaths and directories on the server
 
-const { Server } = require('socket.io');
+const { Server } = require('socket.io'); // socket.io for a bidirectional communication via websockets 
 
 let app = express();
 app.use(express.static('public'));
 
 let httpServer = http.createServer(app);
-
 
 httpServer.listen(PORT, () => {
     Max.outlet("WEB-Server is running");
--- public/client.js
+++ public/client.js
@@ -1,3 +1,8 @@
+/* Since we included the socket.io client code (socket-io.js) 
+   and executed it BEFORE this client.js file, we have now access to socket.io here and can 
+   just use the io() constructor to establish a connection between client and server
+   Socket.io is smart enough to handle the connection process (choosing port etc.) on its own
+*/
 const socket = io();
 
 let playbutton = document.getElementById('playbutton');
